fbe7537dade1cde737d24b60022ed7b741e477bd
woshiwodecode <feelthelight_k@sina.com>
2019-05-05 18:02:54 +0800

    第一次提交/公共部分和轮播图完成

---
+++ src/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- 焦点图 -->
+    <swiper class="banner" autoplay="true" circular="true" indicator-dots  indicator-color="rgba(255, 255, 255, 0.6)" indicator-active-color="#fff">
+      <swiper-item>
+        <image src="/static/uploads/banner1.png"></image>
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner2.png"></image>
+      </swiper-item>
+      <swiper-item>
+        <image src="/static/uploads/banner3.png"></image>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped lang="less">
+.banner {
+    width: 100%;
+    height: 340rpx;
+
+    image {
+      width: 100%;
+      height: 340rpx;
+    }
+  }
+
+</style>
